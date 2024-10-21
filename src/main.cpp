c352d7d7ed88b479fc0ec142be51fb00e77e0619
Nick Porcino <meshula@hotmail.com>
2024-10-20 21:42:02 -0700

    checkpoint dear imgui/2

--- src/main.cpp
+++ src/main.cpp
@@ -175,6 +175,98 @@ void* loop2(void* args) {
     #endif
 }
 
+///-----------------------------------------------------------------------------
+/// ImGui demo
+
+/* I'm using opengl 2 here because it requires less setup and I'm lazy */
+#include "backends/imgui_impl_opengl2.h"
+
+
+/* handle these functions across all apis */
+void imgui_newFrame(void);
+void imgui_render(void);
+void imgui_shutdown(void);
+
+int main_imgui() {
+    RGFW_window* win = RGFW_createWindow("imgui", RGFW_RECT(0, 0, 700, 600), RGFW_CENTER);
+    RGFW_window_makeCurrent(win);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.DeltaTime = 1.0f / 60.0f;
+
+    // Build atlas
+    unsigned char* tex_pixels = nullptr;
+    int tex_w, tex_h;
+    io.Fonts->GetTexDataAsRGBA32(&tex_pixels, &tex_w, &tex_h);
+
+    ImGui_ImplRgfw_InitForOpenGL(win, true);
+    ImGui_ImplOpenGL2_Init();
+
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    while (RGFW_window_shouldClose(win) == RGFW_FALSE) {
+        RGFW_window_checkEvents(win, RGFW_NO_WAIT);
+        io.DisplaySize = ImVec2(win->r.w, win->r.h);
+
+        imgui_newFrame();
+
+        static float f = 0.0f;
+        static int counter = 0;
+        
+        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            char buffer[200];
+            ImGui::InputText("label", buffer, 200);
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+        ImGui::End();
+
+        glViewport(0, 0, win->r.w, win->r.h);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+
+        imgui_render();
+        RGFW_window_swapBuffers(win);
+    }
+
+    imgui_shutdown();
+    RGFW_window_close(win);
+    return EXIT_SUCCESS;
+}
+
+void imgui_newFrame() {
+    ImGui::NewFrame();
+    ImGui_ImplRgfw_NewFrame();
+    ImGui_ImplOpenGL2_NewFrame();
+}
+
+void imgui_render(void) {
+    ImGui::Render();
+
+    ImDrawData* draw_data = ImGui::GetDrawData();
+
+    ImGui_ImplOpenGL2_RenderDrawData(draw_data);
+}
+
+void imgui_shutdown(void) {
+    ImGui_ImplOpenGL2_Shutdown();
+    ImGui_ImplRgfw_Shutdown();
+    ImGui::DestroyContext();
+}
+
 int main(void) {
-    return main_basic();
+    printf("LabTemplate RGFW OpenGL2 demo\n");
+    //return main_basic();
+    return main_imgui();
 }
